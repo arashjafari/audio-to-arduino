0be64c5d23943667e6b5a10a773c6b6851481754
Arash Jafari <arash.jafari@gmail.com>
2024-09-14 12:42:57 +0200

    Convert Pitches to MIDI Note Numbers and Arduino Note Constants

--- audio-to-arduino.py
+++ audio-to-arduino.py
@@ -24,13 +24,25 @@ def extract_pitches(wav_file):
     times = times[~np.isnan(f0)]
     return pitches, times
 
+def frequency_to_midi_note(frequency):
+    return int(round(librosa.hz_to_midi(frequency)))
+
+def midi_note_to_arduino_constant(midi_note):
+    note_names = ['C', 'CS', 'D', 'DS', 'E', 'F', 'FS', 'G', 'GS', 'A', 'AS', 'B']
+    octave = (midi_note // 12) - 1
+    note_index = midi_note % 12
+    note_name = note_names[note_index]
+    return f'NOTE_{note_name}{octave}'
+
 def main():
     mp3_file = 'input.mp3'
     wav_file = 'output.wav'
     convert_mp3_to_wav(mp3_file, wav_file)
     pitches, times = extract_pitches(wav_file)
-    print(pitches)
-    print(times)
+    # print(pitches)
+    # print(times)
+    for freq in pitches:
+        print(midi_note_to_arduino_constant(frequency_to_midi_note(freq)))
 
 if __name__ == '__main__':
     main()
